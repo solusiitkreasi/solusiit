8d6283a845d33c2f74da286f3bdd3648cf4c0da9
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-01-24 09:02:48 +0700

    up

--- resources/views/mail/forget.blade.php
+++
@@ -1,9 +0,0 @@
-<h4>Hello {{$username}},</h4>
-
-<div>
-<p><strong>Your current username:</strong> {{$username}}</p>
-@php
-
-@endphp
-<p><strong>Your new password:</strong> {{$password}}</p>
-</div>
